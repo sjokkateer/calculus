26d89853d9823983cd9fa4e81863c6d65aa2314d
Remy Bos <remy.bos@student.fontys.nl>
2018-11-22 18:59:06 +0100

    Fixed the error in the form (complaint about missing removed buttons and event handlers).

--- eXpressionParsing/eXpressionParsing/Form1.Designer.cs
+++ eXpressionParsing/eXpressionParsing/Form1.Designer.cs
@@ -28,16 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            this.parseBtn = new System.Windows.Forms.Button();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             this.expressionChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.graphPictureBox = new System.Windows.Forms.PictureBox();
-            this.differentiateBtn = new System.Windows.Forms.Button();
             this.parseRbtn = new System.Windows.Forms.RadioButton();
             this.differentiateRbtn = new System.Windows.Forms.RadioButton();
             this.textResultGrpBox = new System.Windows.Forms.GroupBox();
-            this.expressionLb = new System.Windows.Forms.Label();
             this.derivativeLb = new System.Windows.Forms.Label();
+            this.expressionLb = new System.Windows.Forms.Label();
             this.expressionTbx = new System.Windows.Forms.TextBox();
             this.expressionInputLb = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
@@ -47,20 +45,10 @@
             this.textResultGrpBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // parseBtn
-            // 
-            this.parseBtn.Location = new System.Drawing.Point(205, 93);
-            this.parseBtn.Name = "parseBtn";
-            this.parseBtn.Size = new System.Drawing.Size(194, 37);
-            this.parseBtn.TabIndex = 1;
-            this.parseBtn.Text = "Parse Expression";
-            this.parseBtn.UseVisualStyleBackColor = true;
-            this.parseBtn.Click += new System.EventHandler(this.parseBtn_Click);
-            // 
             // expressionChart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.expressionChart.ChartAreas.Add(chartArea1);
+            chartArea4.Name = "ChartArea1";
+            this.expressionChart.ChartAreas.Add(chartArea4);
             this.expressionChart.Location = new System.Drawing.Point(26, 147);
             this.expressionChart.Name = "expressionChart";
             this.expressionChart.Size = new System.Drawing.Size(1268, 907);
@@ -75,21 +63,11 @@
             this.graphPictureBox.TabIndex = 5;
             this.graphPictureBox.TabStop = false;
             // 
-            // differentiateBtn
-            // 
-            this.differentiateBtn.Location = new System.Drawing.Point(405, 93);
-            this.differentiateBtn.Name = "differentiateBtn";
-            this.differentiateBtn.Size = new System.Drawing.Size(265, 37);
-            this.differentiateBtn.TabIndex = 6;
-            this.differentiateBtn.Text = "Differentiate Expression";
-            this.differentiateBtn.UseVisualStyleBackColor = true;
-            this.differentiateBtn.Click += new System.EventHandler(this.differentiateBtn_Click);
-            // 
             // parseRbtn
             // 
             this.parseRbtn.AutoSize = true;
             this.parseRbtn.Checked = true;
-            this.parseRbtn.Location = new System.Drawing.Point(26, 12);
+            this.parseRbtn.Location = new System.Drawing.Point(26, 15);
             this.parseRbtn.Name = "parseRbtn";
             this.parseRbtn.Size = new System.Drawing.Size(99, 29);
             this.parseRbtn.TabIndex = 8;
@@ -100,7 +78,7 @@
             // differentiateRbtn
             // 
             this.differentiateRbtn.AutoSize = true;
-            this.differentiateRbtn.Location = new System.Drawing.Point(26, 50);
+            this.differentiateRbtn.Location = new System.Drawing.Point(26, 53);
             this.differentiateRbtn.Name = "differentiateRbtn";
             this.differentiateRbtn.Size = new System.Drawing.Size(159, 29);
             this.differentiateRbtn.TabIndex = 9;
@@ -118,15 +96,6 @@
             this.textResultGrpBox.TabStop = false;
             this.textResultGrpBox.Text = "Text results";
             // 
-            // expressionLb
-            // 
-            this.expressionLb.AutoSize = true;
-            this.expressionLb.Location = new System.Drawing.Point(25, 47);
-            this.expressionLb.Name = "expressionLb";
-            this.expressionLb.Size = new System.Drawing.Size(131, 25);
-            this.expressionLb.TabIndex = 0;
-            this.expressionLb.Text = "Expression: ";
-            // 
             // derivativeLb
             // 
             this.derivativeLb.AutoSize = true;
@@ -136,11 +105,20 @@
             this.derivativeLb.TabIndex = 1;
             this.derivativeLb.Text = "Derivative: ";
             // 
+            // expressionLb
+            // 
+            this.expressionLb.AutoSize = true;
+            this.expressionLb.Location = new System.Drawing.Point(25, 47);
+            this.expressionLb.Name = "expressionLb";
+            this.expressionLb.Size = new System.Drawing.Size(131, 25);
+            this.expressionLb.TabIndex = 0;
+            this.expressionLb.Text = "Expression: ";
+            // 
             // expressionTbx
             // 
             this.expressionTbx.Location = new System.Drawing.Point(205, 42);
             this.expressionTbx.Name = "expressionTbx";
-            this.expressionTbx.Size = new System.Drawing.Size(671, 31);
+            this.expressionTbx.Size = new System.Drawing.Size(1089, 31);
             this.expressionTbx.TabIndex = 0;
             // 
             // expressionInputLb
@@ -154,11 +132,11 @@
             // 
             // processBtn
             // 
-            this.processBtn.Location = new System.Drawing.Point(903, 42);
+            this.processBtn.Location = new System.Drawing.Point(205, 83);
             this.processBtn.Name = "processBtn";
-            this.processBtn.Size = new System.Drawing.Size(276, 73);
+            this.processBtn.Size = new System.Drawing.Size(1089, 49);
             this.processBtn.TabIndex = 13;
-            this.processBtn.Text = "Process";
+            this.processBtn.Text = "Process Expression";
             this.processBtn.UseVisualStyleBackColor = true;
             this.processBtn.Click += new System.EventHandler(this.processBtn_Click);
             // 
@@ -172,10 +150,8 @@
             this.Controls.Add(this.expressionInputLb);
             this.Controls.Add(this.differentiateRbtn);
             this.Controls.Add(this.parseRbtn);
-            this.Controls.Add(this.differentiateBtn);
             this.Controls.Add(this.graphPictureBox);
             this.Controls.Add(this.expressionChart);
-            this.Controls.Add(this.parseBtn);
             this.Controls.Add(this.expressionTbx);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -189,10 +165,8 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button parseBtn;
         private System.Windows.Forms.DataVisualization.Charting.Chart expressionChart;
         private System.Windows.Forms.PictureBox graphPictureBox;
-        private System.Windows.Forms.Button differentiateBtn;
         private System.Windows.Forms.RadioButton parseRbtn;
         private System.Windows.Forms.RadioButton differentiateRbtn;
         private System.Windows.Forms.GroupBox textResultGrpBox;
